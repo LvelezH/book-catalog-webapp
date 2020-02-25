28516483b244404607ffb9ebe6ad680e9022c928
LvelezH <tienemanitas@gmail.com>
2020-02-25 21:05:43 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './authentication/login.component';
+import {AuthenticationGuard} from './authentication/AuthenticationGuard';
+import {CatalogComponent} from './catalog/catalog.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'catalog', component: CatalogComponent, canActivate: [ AuthenticationGuard ] },
+  { path: '', redirectTo: '/catalog', pathMatch: 'full' },
+  { path: '**', redirectTo: '/catalog'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
